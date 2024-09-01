19f5dbaa094c1cc726c396d1dd90c16f32d88b66
Hwiwon Kang <syaron9364@gmail.com>
2024-09-01 19:57:30 +0900

    notfound, bg modified

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import "./NotFoundPage.css";
+import { useNavigate } from "react-router-dom";
 
-const NotFoundPage = () => {
-  return <div>NotFoundPage Rendering</div>;
-};
+function NotFoundPage() {
+  const navigate = useNavigate();
+  const goToHome = () => {
+    navigate("/");
+  };
+  return (
+    <div className="notfound-container">
+      <h1 className="notfound-title">Lost your way?</h1>
+      <p className="notfound-text">
+        Sorry, we can't find that page. You'll find lots to explore on the home
+        page.
+      </p>
+      <button className="notfound-button" onClick={goToHome}>
+        Netflix Home
+      </button>
+    </div>
+  );
+}
 
 export default NotFoundPage;
